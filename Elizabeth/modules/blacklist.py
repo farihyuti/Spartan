cd4b688b0d0ec851ba883a5034611ac3aef94fa0
Clara <79531054+farihyuti@users.noreply.github.com>
2021-03-10 15:45:46 +0700

    Update blacklist.py

--- Elizabeth/modules/blacklist.py
+++ Elizabeth/modules/blacklist.py
@@ -461,7 +461,7 @@ Blacklists are used to stop certain triggers from being said in a group. Any tim
 *NOTE*: Blacklists do not affect group admins.
 
  ➩ /blacklist: View the current blacklisted words.
-
+Have A bug? Visit @vohaunion
 Admin only:
  ➩ /addblacklist <triggers>: Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
  ➩ /unblacklist <triggers>: Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
@@ -469,7 +469,7 @@ Admin only:
  ➩ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>: Action to perform when someone sends blacklisted words.
 """
 
-__mod_name__ = "⚜ B. LIST ⚜"
+__mod_name__ = "blacklist"
 
 
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
